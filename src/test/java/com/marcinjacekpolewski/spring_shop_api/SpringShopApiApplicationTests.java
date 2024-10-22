70ab76e34033dcbdc05dfd3ddfbd3f005bd39ae3
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-10-22 08:41:54 +0200

    initial commit

---
+++ src/test/java/com/marcinjacekpolewski/spring_shop_api/SpringShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcinjacekpolewski.spring_shop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
